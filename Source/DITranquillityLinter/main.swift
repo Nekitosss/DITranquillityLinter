32360b21a001260cdcd1444e34e202b33ae512e1
Nikita <patskovn@yahoo.com>
2018-09-30 22:13:43 +0300

    update xcodeproj package version

--- Source/DITranquillityLinter/main.swift
+++ Source/DITranquillityLinter/main.swift
@@ -7,7 +7,7 @@ import Basic
 
 func projectFiles(project: XcodeProj, srcRoot: String) -> [URL] {
 	let srcAbsolute = AbsolutePath(srcRoot)
-	let sourceFileReferences = project.pbxproj.objects.sourcesBuildPhases.flatMap({ $0.value.files.compactMap({ $0.file }) })
+	let sourceFileReferences = project.pbxproj.sourcesBuildPhases.flatMap({ $0.files.compactMap({ $0.file }) })
 	return sourceFileReferences.compactMap({ (element: PBXFileElement) -> URL? in
 		guard let fullPath = (try? element.fullPath(sourceRoot: srcAbsolute))??.asString else { return nil }
 		return URL(string: fullPath)
@@ -18,12 +18,12 @@ DispatchQueue.global().async {
 	let tokenizer = Tokenizer()
 	
 	let srcRoot = "/Users/nikita/development/DITranquillityLinter/LintableProject/"
-	let workspace = try! XCWorkspace(pathString: srcRoot + "LintableProject.xcworkspace")
 	let project = try! XcodeProj(pathString: srcRoot + "LintableProject.xcodeproj")
 	let podsProject = try! XcodeProj(pathString: srcRoot + "Pods/Pods.xcodeproj")
 	let paths = projectFiles(project: project, srcRoot: srcRoot) + projectFiles(project: podsProject, srcRoot: srcRoot)
 	tokenizer.process(files: paths)
 	print("end")
+	exit(EXIT_SUCCESS)
 }
 
 dispatchMain()
