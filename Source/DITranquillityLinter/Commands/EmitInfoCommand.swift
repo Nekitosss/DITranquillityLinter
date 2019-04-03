747a615c2ec74eb108ac488677d4ef6907a594fa
Nikita Patskov <patskovn@yahoo.com>
2019-04-03 15:36:02 +0300

    [WIP] Create emit info command

--- Source/DITranquillityLinter/Commands/EmitInfoCommand.swift
+++ Source/DITranquillityLinter/Commands/EmitInfoCommand.swift
@@ -10,9 +10,7 @@ import Commandant
 import Dispatch
 import Foundation
 import DITranquillityLinterFramework
-import xcodeproj
 import PathKit
-import Yams
 
 
 struct EmitInfoCommand: CommandProtocol {
@@ -21,23 +19,20 @@ struct EmitInfoCommand: CommandProtocol {
 	
 	let function = "Creates container info for further linting"
 	
-	private let projectFileExtractor = ProjectFileExtractor()
+	private let fileCollector = FileCollector()
 	
 	func run(_ options: OptionalLintOptions) -> Result<(), CommandantError<()>> {
-		LintOptions.shared = extractOptionsFromYMLIfProvided(options: options)
+		LintOptions.shared = options.extractOptionsFromYMLIfProvided()
+		guard let outputPath = options.outputPath else {
+			return .failure(.usageError(description: "Output path for not provided. Please specify it using '-o' option."))
+		}
+		
 		do {
 			let infoEmitter = ContainerInfoEmitter(isTestEnvironment: true)
-			let srcRoot = XcodeEnvVariable.srcRoot.value() ?? XcodeEnvVariable.srcRoot.defaultValue
-			let defaultProjectFile = EnvVariable.testableProjectFolder.value() + EnvVariable.testableProjectName.value()
-			let mainProjectPath = XcodeEnvVariable.projectFilePath.value() ?? defaultProjectFile
-			let project = try XcodeProj(pathString: mainProjectPath)
+			let files = try fileCollector.collectSourceFiles()
 			
-			TimeRecorder.start(event: .collectSource)
-			let files = projectFileExtractor.extractProjectFiles(from: project, srcRoot: srcRoot)
-			TimeRecorder.end(event: .collectSource)
-			
-			let outputPath = FileManager.default.currentDirectoryPath + "/\(XcodeEnvVariable.productName.value() ?? "module")_di_graph_info"
-			let successed = try infoEmitter.process(files: Array(Set(files)), outputFilePath: URL(string: outputPath)!)
+			let normalizedOutput = Path(outputPath).normalize().url
+			let successed = try infoEmitter.process(files: files, outputFilePath: normalizedOutput)
 			TimeRecorder.end(event: .total)
 			if successed {
 				return .success(())
@@ -46,28 +41,8 @@ struct EmitInfoCommand: CommandProtocol {
 			}
 		} catch {
 			Log.error("Error during file parsing \(error)")
-			return .failure(.usageError(description: "Error occured during linting: \(error)"))
+			return .failure(.usageError(description: "Error occured during file creation: \(error)"))
 		}
-	}
-	
-	
-	private func extractOptionsFromYMLIfProvided(options: OptionalLintOptions) -> LintOptions {
-		var options = options
-		do {
-			if let ymlPath = options.yml.map(URL.init(fileURLWithPath:)) {
-				let data = try Data(contentsOf: ymlPath)
-				let encodedYaml = String(data: data, encoding: .utf8) ?? ""
-				let decoder = YAMLDecoder()
-				let decodedOptions: OptionalLintOptions = try decoder.decode(from: encodedYaml)
-				options.logLevel = options.logLevel ?? decodedOptions.logLevel
-				options.commonCachePath = options.commonCachePath ?? decodedOptions.commonCachePath
-				options.localCachePath = options.localCachePath ?? decodedOptions.localCachePath
-				options.shouldRecordTime = (options.shouldRecordTime ?? false) || (decodedOptions.shouldRecordTime ?? false)
-			}
-		} catch {
-			Log.error(error)
-		}
-		return LintOptions(logLevel: options.logLevel, commonCachePath: options.commonCachePath, localCachePath: options.localCachePath, shouldRecordTime: options.shouldRecordTime ?? false)
 	}
 	
 }
---
+++ Source/DITranquillityLinter/OptionalLintOptions.swift
@@ -0,0 +1,57 @@
+
+import Foundation
+import Result
+import Commandant
+import DITranquillityLinterFramework
+import Yams
+
+struct OptionalLintOptions: Codable {
+	var logLevel: String?
+	var commonCachePath: String?
+	var localCachePath: String?
+	var shouldRecordTime: Bool?
+	var yml: String?
+	var outputPath: String?
+	
+	func extractOptionsFromYMLIfProvided() -> LintOptions {
+		var options = self
+		do {
+			if let ymlPath = options.yml.map(URL.init(fileURLWithPath:)) {
+				let data = try Data(contentsOf: ymlPath)
+				let encodedYaml = String(data: data, encoding: .utf8) ?? ""
+				let decoder = YAMLDecoder()
+				let decodedOptions: OptionalLintOptions = try decoder.decode(from: encodedYaml)
+				options.logLevel = options.logLevel ?? decodedOptions.logLevel
+				options.commonCachePath = options.commonCachePath ?? decodedOptions.commonCachePath
+				options.localCachePath = options.localCachePath ?? decodedOptions.localCachePath
+				options.shouldRecordTime = (options.shouldRecordTime ?? false) || (decodedOptions.shouldRecordTime ?? false)
+			}
+		} catch {
+			Log.error(error)
+		}
+		return LintOptions(logLevel: options.logLevel, commonCachePath: options.commonCachePath, localCachePath: options.localCachePath, shouldRecordTime: options.shouldRecordTime ?? false, outputPath: options.outputPath)
+	}
+}
+
+
+extension OptionalLintOptions: OptionsProtocol {
+	
+	public static func create(_ logLevel: String?) -> (_ commonCachePath: String?) -> (_ localCachePath: String?) -> (_ shouldRecordTime: Bool?) -> (_ yml: String?) -> (_ outputPath: String?) -> OptionalLintOptions {
+		return { commonCachePath in { localCachePath in { shouldRecordTime in { yml in { outputPath in
+			//			let level = Log.Level(rawValue: logLevel) ?? Log.Level.warnings
+			return OptionalLintOptions(logLevel: logLevel, commonCachePath: commonCachePath, localCachePath: localCachePath, shouldRecordTime: shouldRecordTime, yml: yml, outputPath: outputPath)
+			} } } } }
+	}
+	
+	public static func evaluate(_ m: CommandMode) -> Result<OptionalLintOptions, CommandantError<CommandantError<()>>> {
+		return create
+			<*> m <| Option<String?>(key: "log-level", defaultValue: nil, usage: "level for log printing")
+			<*> m <| Option<String?>(key: "common-cache-path", defaultValue: nil, usage: "path to common binary (UIKit, Cocoa, Foundation) parsing cache")
+			<*> m <| Option<String?>(key: "local-cache-path", defaultValue: nil, usage: "path to project-related binary parsing cache")
+			<*> m <| Option<Bool>(key: "record-time", defaultValue: false, usage: "log elapsed time")
+			<*> m <| Option<String?>(key: "yml", defaultValue: ".dilint.yml", usage: "path to yml configuration file")
+			<*> m <| Option<String?>(key: "o", defaultValue: "", usage: "path to output file")
+		
+	}
+	
+}
